0110fdb556cbb0b2d003b0c335d754d149033e3f
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-24 07:59:35 +0100

    Delete data.jsj

--- data.js
+++
@@ -1,9 +0,0 @@
-window.WyteData = {
-  defaultPresets: [
-    { name: 'Standard Web App', package: 'com.wyte.webapp', url: 'https://example.com' },
-    { name: 'E-Commerce TWA', package: 'com.wyte.shop', url: 'https://shop.example.com' }
-  ],
-  permissionsList: [
-    'INTERNET', 'CAMERA', 'MICROPHONE', 'STORAGE', 'LOCATION', 'VIBRATION'
-  ]
-};
